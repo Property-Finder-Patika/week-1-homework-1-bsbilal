a2defe8b47a9baf732c9479a857f5efd67101766
Basulas Bilal <bilal530@gmaill.com>
2022-06-24 20:22:06 +0300

    3.1 ok

---
+++ book/ch3_Basic_Data_Types/3.1Integers.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	/*
+		var u uint8 = 255
+		fmt.Println(u, u+1, u*u) // "255 0 1"
+		var i int8 = 127
+		fmt.Println(i, i+1, i*i) // "127 -128 1",
+	*/
+
+	var x uint8 = 1<<1 | 1<<5
+	var y uint8 = 1<<1 | 1<<2
+	fmt.Printf("%08b\n", x)    // "00100010", the set {1, 5}
+	fmt.Printf("%08b\n", y)    // "00000110", the set {1, 2}
+	fmt.Printf("%08b\n", x&y)  // "00000010", the intersection {1}
+	fmt.Printf("%08b\n", x|y)  // "00100110", the union {1, 2, 5}
+	fmt.Printf("%08b\n", x^y)  // "00100100", the symmetric difference {2, 5}
+	fmt.Printf("%08b\n", x&^y) // "00100000", the difference {5}
+	for i := uint(0); i < 8; i++ {
+		if x&(1<<i) != 0 { // membership test
+			fmt.Println(i) // "1", "5"
+		}
+	}
+	fmt.Printf("%08b\n", x<<1) // "01000100", the set {2, 6}
+	fmt.Printf("%08b\n", x>>1) // "00010001", the
+
+	//
+	for x := 0; x < 8; x++ {
+		fmt.Printf("x = %d e A = %8.3f\n", x, math.Exp(float64(x)))
+	}
+}
